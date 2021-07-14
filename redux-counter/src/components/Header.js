9da8b8132eba952af67c191c72420e63c96e17eb
David Riddervold Marconis <david@marconis.no>
2021-07-14 13:04:33 +0200

    Add redux project with multiple slices

---
+++ redux-counter/src/components/Header.js
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import { authenticationActions } from "../store/authentication";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  const isAuthenticated = useSelector(
+    state => state.authentication.isAuthenticated
+  );
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authenticationActions.logout());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      <nav>
+        {isAuthenticated && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ redux-counter/src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authenticationReducer from "./authentication";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    authentication: authenticationReducer
+  }
+});
+
+export default store;
